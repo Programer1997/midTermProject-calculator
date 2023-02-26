2aa20f3e2a41d6c6fa1503302ef424ab052ba19e
abraham velazquez <wero210697@gmail.com>
2023-02-26 13:45:58 -0500

    commit to upload all the new changes in CSS and also fixed problems in JS

--- calculator.js
+++ calculator.js
@@ -7,23 +7,24 @@ function GEB(){
 var age= document.getElementById('age').value;
 var weight = document.getElementById('weight').value;
 var height= document.getElementById('height').value;
-//var activityLevel = document.getElementById('activityLevel').value;
+var activityLevel = document.getElementById('activity').value;
 var gender = document.getElementById('gender').value;
 
 var result =0;
  
 
     if (gender=="man"){
-        result = 66.5 +(13.75*weight)+ (5*height)-(6.78*age);
+        result = (66.5 +(13.75*weight)+ (5*height)-(6.78*age))*activityLevel;
 
     }else if (gender=="women"){
-        result = 665 +(9.6*weight)+ (1.85*height)-(4.68*age);
+        result = (665 +(9.6*weight)+ (1.85*height)-(4.68*age))*activityLevel;
     }else {
 		alert("Selecciona tu género.");
 		return;
 	}
 
     var resultadoDiv = document.getElementById("resultFinal");
+   
 	resultadoDiv.innerHTML = "<p>Tu GEB es: <strong>" + result.toFixed(2) + " calorías</strong></p>";
 
     
